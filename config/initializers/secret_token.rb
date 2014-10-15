74f938750d564dbd4ce6ac468940146c1f164048
lejspy <lejspy@gmail.com>
2014-10-15 15:43:38 +0200

    commit hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workshop::Application.config.secret_token = 'd1259757cd4b804a95f05e3c9a1fe5927451507a8bf0185542e33e24d7c9bc1f94ed60bcc5b807180c953777faa19ab7466802a79d6997b20aae348710628a91'
